cae359210a69be7e14f08bc8689d361505129c8e
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-20 17:42:35 +0530

    diff approach

--- 7.py
+++ 7.py
@@ -14,7 +14,7 @@ marks=myArray(10,5)
 marks.set_values()
 marks.show_values() """
 
-nums=list(map(int,input("enter the numbers:").split()))
+nums=list(map(int,input("enter the numbers:").split()))    #using O(n^2) tc using normal arrays
 found=False
 for i in range(len(nums)):
     for j in range(i+1,len(nums)):
@@ -24,3 +24,17 @@ for i in range(len(nums)):
             break
 if not found:
     print("False")
+
+nums=[1,2,3,4,5,1]
+hashset=set()             #using hashset O(n) tc 
+found=False
+for i in range(len(nums)):
+    if nums[i] in hashset:
+        print("True")
+        found=True
+        break
+    else:
+        hashset.add(nums[i])
+if not found:
+    print("False")
+print(hashset)
