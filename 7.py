961b24f65c84adbf9384fe012f89ef14c1651236
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-20 17:11:42 +0530

    Day 7

---
+++ 7.py
@@ -0,0 +1,16 @@
+class myArray:
+    def __init__(self,totalsize,usedsize):
+        self.totalsize=totalsize
+        self.usedsize=usedsize
+        self.ptr=[0]*totalsize
+    def set_values(self):
+        for i in range(self.usedsize):
+            num=int(input("enter the number:\n"))
+            self.ptr[i]=num
+    def show_values(self):
+        print(self.ptr[:self.usedsize])
+
+marks=myArray(10,5)
+marks.set_values()
+marks.show_values()
+
