31919c1a3fa1dcae2e7e1de5cd372e5a0587e3ec
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-21 19:06:39 +0530

    Anagram

---
+++ 9.py
@@ -0,0 +1,8 @@
+#anagram using sorted function
+s="listen"
+t="silent"                       #using sorted function (very easy one)  O(1) i guessss
+if sorted(t)==sorted(s):
+    print("True")
+else:
+    print("False")
+
