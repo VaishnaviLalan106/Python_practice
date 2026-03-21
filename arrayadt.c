da0713027263996af25c7c3ac116e453a8b1601d
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-21 14:45:10 +0530

    C programmm

---
+++ arrayadt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+struct arrayadt
+{
+    int total_size;
+    int used_size;
+    int *ptr;
+};
+
+void createArray(struct arrayadt* a){
+
+}
+int main()
+{
+    struct arrayadt marks;
+    return 0;
+}
