1a56d4171891a4b1d66a646540e08057f9bd58df
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-23 21:56:03 +0530

    Two sum

--- 12.py
+++ 12.py
@@ -3,15 +3,27 @@ print("input list is:", nums)
 print(len(nums))
 k=int(input("Enter target sum:"))
 print(f"k={k}")
+"""
+sort=sorted(nums)
+print("sorted list is:", sort)
+k=int(input("Enter target sum:"))
+print(f"k={k}")                 
 found=False
-for i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-        if nums[i]+nums[j]==k :
+for i in range(len(sort)):           #brute force O(n^2) time complexity
+    for j in range(i+1,len(sort)):
+        if sort[i]+sort[j]==k :
             print([i,j])
             found=True
             break
 if not found:
-    print("bye")
+    print("bye") """
 
-        
+
+hash={}                  #using O(n) time complexity 
+for i , values in enumerate(nums):
+      diff=k-values
+      if diff in hash:
+        print([hash[diff],i])
+      hash[values]=i
+
 
