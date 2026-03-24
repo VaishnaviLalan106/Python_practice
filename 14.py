855b8e3ccb946b6f91df739eaea3aa7e9ce8589e
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-24 14:56:37 +0530

    anagram

--- 14.py
+++ 14.py
@@ -3,3 +3,15 @@ b=[3,4,5,6]
 int1=set(a)
 int2=set(b)
 print([i for i in int1 if i in int2])
+
+#anagrams
+strs=["ate","listen","tan","nat","silent","eat","tea"]
+group={}
+for s in strs:
+    key=''.join(sorted(s))
+    if key in group:
+        group[key].append(s)
+    else:
+        group[key]=[s]
+print(group)
+print(group.values())
