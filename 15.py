238877808babbe61089908e52a8d5d1079e0d0fa
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-25 21:17:14 +0530

    Bucket sort

--- 15.py
+++ 15.py
@@ -1,6 +1,6 @@
-arr=[1,1,2,2,3,4]
+arr=[1,1,1,2,2,3,4]
 k=2
-group={}
+group={}             # as this is true but sorted takes O(nlogn) tc 
 for i in arr:
     if i in group:
         group[i]+=1
@@ -8,5 +8,23 @@ for i in arr:
         group[i]=1
 print(group)
 for key,values in group.items():
-    if values==k:
-        print([key])
+    if values>1:
+        print([key]) 
+        
+#for top k most frequent element use bucket sort  O(n)
+arr=[1,1,1,2,2,3,4]
+k=2
+group={} 
+for i in arr:
+    group[i]=group.get(i,0)+1
+print(group)
+bucket=[[] for i in range(len(arr)+1)]
+for key, values in group.items():
+    bucket[values].append(key)
+print(bucket)
+result=[]
+for i in range(len(bucket)-1,0,-1):
+    for i in bucket[i]:
+        result.append(i)
+        if len(result)==k:
+            print(result)
