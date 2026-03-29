ca3299f763e0c76369f02d8fd4da2d128208081e
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-29 18:49:24 +0530

    Second largest

--- 18.py
+++ 18.py
@@ -1,4 +1,4 @@
-#encode
+""" #encode
 word="Hello, World!!"
 strs=word.split(" ")
 print(strs)
@@ -18,4 +18,24 @@ while i < len(strs):
     length=int(strs[i:j])
     res.append(strs[j+1:j+1+length])
     i=j+1+length
-print(res)
+print(res) """
+
+#second largest
+nums=list(map(int,input("Enter the numbers separated by space:").split()))
+nums=list(set(nums))
+print(nums)
+max1=nums[0]
+for i in range(len(nums)):
+    if nums[i]>max1:
+        max1=nums[i]
+print(max1)
+nums.remove(max1)
+print(nums)
+max2=nums[0]
+for i in range(len(nums)):
+    if nums[i]>max2:
+        max2=nums[i]
+print(max2)
+
+   
+ 
