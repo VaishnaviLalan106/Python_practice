f75cc56566f15d373b8a960c68c126721f93b4de
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-30 15:42:01 +0530

    Day 19

---
+++ 19.py
@@ -0,0 +1,9 @@
+#longest consecutive sequence
+unsortedarr=[100,4,200,1,3,2]
+nums=sorted(unsortedarr)
+print(nums)
+arr=[nums[0]]
+for i in range(0,len(nums)-1):
+    if nums[i]+1==nums[i+1]:
+        arr.append(nums[i+1])
+print(arr)
