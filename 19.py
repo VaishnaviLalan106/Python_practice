645aaa66b223f6df698e3fbaffa90fd0fb0e9250
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-30 16:14:51 +0530

    Okay byee

--- 19.py
+++ 19.py
@@ -1,9 +1,11 @@
 #longest consecutive sequence
-unsortedarr=[100,4,200,1,3,2]
-nums=sorted(unsortedarr)
+unsortedarr=[76,9,8,8,0,3,4,23,1,5,3,5,7,6]
+print(unsortedarr)
+nums=sorted(set(unsortedarr))
 print(nums)
 arr=[nums[0]]
 for i in range(0,len(nums)-1):
     if nums[i]+1==nums[i+1]:
         arr.append(nums[i+1])
 print(arr)
+print(len(arr))
