9f912c9cd06a3201ce9d56e29e032cb3f2371f39
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-30 17:38:19 +0530

    Forgot this

--- 19.py
+++ 19.py
@@ -1,4 +1,4 @@
-""" #longest consecutive sequence
+#longest consecutive sequence
 unsortedarr=[76,9,8,8,0,3,4,23,1,5,3,5,7,6]
 print(unsortedarr)
 nums=sorted(set(unsortedarr))
@@ -8,10 +8,10 @@ for i in range(0,len(nums)-1):
     if nums[i]+1==nums[i+1]:
         arr.append(nums[i+1])
 print(arr)
-print(len(arr)) """
+print(len(arr)) 
 
 
-""" #longest word 
+#longest word 
 sent="I love: programming, so so much"
 word=list(sent.split())
 print(word)
@@ -21,7 +21,7 @@ for i in range(len(word)):
     if len(word[i])>len(longest):
         longest=word[i]
 print(longest,len(longest))
- """
+ 
 
 #to count words
 sent="hellllooo, there   how are     youuu??"
