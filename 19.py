916561ce15e14e6ecb235feb714248ae2ec38be8
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-30 17:18:26 +0530

    Okay longest word

--- 19.py
+++ 19.py
@@ -1,4 +1,4 @@
-#longest consecutive sequence
+""" #longest consecutive sequence
 unsortedarr=[76,9,8,8,0,3,4,23,1,5,3,5,7,6]
 print(unsortedarr)
 nums=sorted(set(unsortedarr))
@@ -8,4 +8,16 @@ for i in range(0,len(nums)-1):
     if nums[i]+1==nums[i+1]:
         arr.append(nums[i+1])
 print(arr)
-print(len(arr))
+print(len(arr)) """
+
+
+#longest word 
+sent="I love: programming, so so much"
+word=list(sent.split())
+print(word)
+longest=""
+for i in range(len(word)):
+    word[i]=word[i].strip(",:.")
+    if len(word[i])>len(longest):
+        longest=word[i]
+print(longest,len(longest))
