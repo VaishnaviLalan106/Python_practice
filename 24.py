0bb5f3a4630aa583d654273a5356d0d08aa4bc40
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-04-08 12:10:45 +0530

    okay rain water

--- 24.py
+++ 24.py
@@ -33,3 +33,17 @@ while l<r:
     else:
         l+=1
 print(f"container with most water is {res}")
+
+
+#trapping rain water 
+height=[0,1,0,2,1,0,1,3,2,1,2,1]
+res=0
+for i in range(len(height)):
+    lmax=0
+    rmax=0
+    for j in range(i):
+        lmax=max(lmax,height[j])
+    for j in range(i+1,len(height)):
+        rmax=max(rmax,height[j])
+    res+=min(lmax,rmax)-height[i]
+print(res)
